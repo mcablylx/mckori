64cec393cc97d41934d95e580b337b904db05eac
lk6142916 <7888827@qq.com>
2021-07-01 15:19:42 +0800

    first
    first

---
+++ mylibrary/src/main/java/top/mcablylx/kori/Test.kt
@@ -0,0 +1,12 @@
+package top.mcablylx.kori
+
+/**
+ * If there is no bug, Created by Mcablylx on 2021/07/01.
+ * otherwise, I do not know who create it either
+ * If anyone finds out I have bug, I can only kill him quietly
+ */
+class Test {
+    fun test(){
+        print("222")
+    }
+}
