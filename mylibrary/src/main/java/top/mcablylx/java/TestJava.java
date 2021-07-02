cab34a9e455ac1599adef413e7bf30f1619a5a0e
lk6142916 <7888827@qq.com>
2021-07-03 00:39:00 +0800

    Create TestJava.java

---
+++ mylibrary/src/main/java/top/mcablylx/java/TestJava.java
@@ -0,0 +1,12 @@
+package top.mcablylx.java;
+
+/**
+ * If there is no bug, Created by Mcablylx on 2021/07/03.
+ * otherwise, I do not know who create it either
+ * If anyone finds out I have bug, I can only kill him quietly
+ */
+public class TestJava {
+    public void test(){
+        System.out.println("test java");
+    }
+}
